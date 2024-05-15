f3eb68024f3c2d88621c42622739175c1161ea22
Nitya Ravi <nityarvi@gmail.com>
2024-05-15 14:26:09 -0400

    added function to find stellar mass weighted stellar velocity dispersion

--- ellipticals/elliptical_virial_mass.py
+++ ellipticals/elliptical_virial_mass.py
@@ -14,6 +14,115 @@ c = 299792.458 # Speed of light in units of km/s
 ################################################################################
 
 
+def sMass_weighted_star_sigma(gal_ID, star_sigma, star_sigma_ivar, 
+                              star_sigma_corr, star_sigma_mask, sMass_density,
+                              sMass_density_err, IMAGE_DIR=None, 
+                              IMAGE_FORMAT='png'):
+    
+    '''
+
+    Apply correction and mask to stellar velocity dispersion map and 
+    calculate stellar mass weighted mean, uncertainty
+
+    PARAMETERS
+    ==========
+    gal_ID : string
+        galaxy plate-ifu
+
+    star_sigma : array
+        stellar velocity dispersion map [km/s]
+    
+    star_sigma_ivar : array
+        stellar velocity dispersion inverse variance map
+
+    star_sigma_corr : array
+        stellar velocity dispersion correction map
+
+    star_sigma_mask : array
+        stellar velocity dispersion default mask
+
+    sMass_density : array
+        stellar mass density map [log(M_sun/spax^2)]
+
+    sMass_density_err : array
+        stellar mass density uncertainty map
+    
+    IMAGE_DIR : string
+        directory to save figures, default is None
+
+    IMAGE_FORMAT : string
+        format to save figures, deafault is 'png'
+
+    RETURNS
+    =======
+    x2 : float
+        stellar mass weighted mean square velocity dispersion [(km/s)^2]
+
+    x2_err2 : float
+        square of the uncertainty on x2
+
+    mstar_sigma : array
+        masked stellar velocity dispersion map
+
+    mstar_sigma_corrected : array
+        masked and corrected stellar velocity dispersion map
+
+
+
+    '''
+
+    ############################################################################
+    # mask all maps
+    ############################################################################
+
+    
+    sn10_mask = np.logical_or(star_sigma * 
+                               np.sqrt(star_sigma_ivar) < 10, 
+                               star_sigma_mask)
+
+    mstar_sigma = ma.array(star_sigma, mask=sn10_mask)
+    mstar_sigma_ivar = ma.array(star_sigma_ivar, mask=sn10_mask)
+    mstar_sigma_corr = ma.array(star_sigma_corr, mask=sn10_mask)
+
+    msMass_density = ma.array(sMass_density, mask=sn10_mask)
+    msMass_density_err = ma.array(sMass_density_err, mask=sn10_mask)
+
+    ############################################################################
+    # apply correction to stellar velocity dispersion map and plot
+    ############################################################################
+
+    mstar_sigma_corrected = np.sqrt(mstar_sigma**2 - mstar_sigma_corr**2)
+    plot_sigma_map(gal_ID, mstar_sigma, IMAGE_DIR=IMAGE_DIR, IMAGE_FORMAT=IMAGE_FORMAT)
+    plot_sigma_map(gal_ID, mstar_sigma_corrected, corr=True, IMAGE_DIR=IMAGE_DIR, 
+                   IMAGE_FORMAT=IMAGE_FORMAT)
+    
+    ############################################################################
+    # calculate stellar mass weighted mean velocity dispersion
+    ############################################################################
+
+    # sMass = np.ma.power(10,msMass_density) # stellar mass map in linear units
+    # sMass_tot = ma.sum 
+    # sigma2 = ma.sum(sMass*mstar_sigma_corrected**2)/ma.sum(sMass)
+    # sigma = np.sqrt(sigma2)
+    # return sigma, 0, mstar_sigma, mstar_sigma_corrected
+
+
+    lin_sMass = np.ma.power(10, msMass_density) # stellar mass map in linear units
+    lin_sMass_err = np.ma.power(10, msMass_density_err) # stellar mass error in linear units
+
+
+    M = ma.sum(lin_sMass) # total stellar mass, normalization factor
+    S = ma.sum(lin_sMass*mstar_sigma_corrected**2) # weighted sum of vel disp squared
+
+    # mass weighted mean velocity disp squared and square of uncertainty on this quantity
+    x2 = S / M 
+    x2_err2 = (4 / M**2) * \
+        ma.sum((lin_sMass*mstar_sigma_corrected)**2/mstar_sigma_ivar) +\
+        (1 / M**4) * ma.sum(((M*mstar_sigma_corrected**2-S)*lin_sMass_err)**2)
+    
+    return x2, x2_err2, mstar_sigma, mstar_sigma_corrected
+
+
 
 
 def median_star_sigma(gal_ID, star_sigma, star_sigma_ivar, 
@@ -23,12 +132,57 @@ def median_star_sigma(gal_ID, star_sigma, star_sigma_ivar,
     Apply correction and mask to stellar velocity dispersion map and find 
     median, uncertainty
 
+    PARAMETERS
+    ==========
+
+    gal_ID : string
+        MaNGA plate-IFU
+
+    star_sigma : array
+        stellar velocity dispersion map
+
+    star_sigma_ivar : array
+        stellar velocity dispersion inverse variance
+
+    star_sigma_corr : array
+        stellar velocity dispersion instrument correction
+
+    star_sigma_mask : array
+        stellar velocity dispersion default mask
+
+    IMAGE_DIR : string
+        directory to save plots
+
+    IMAGE_FORMAT : string
+        format to save plots. default is 'png'
+
+    RETURNS
+    =======
+    star_sigma_med : float
+        median stellar velocity dispersion calculated from masked map
+
+    star_sigma_med_err : float
+        error on star_sigma_med
+
+    mstar_sigma : array
+        masked stellar velocity dispersion map
+
+    mstar_sigma_corr : array
+        masked stellar velocity dispersion correction map
+          mstar_sigma, mstar_sigma_corr
+
+
 
 
     '''
     ############################################################################
     # mask all maps
     ############################################################################
+
+    
+    # sn10_mask = np.logical_or(maps['star_sigma'] * 
+    #                           np.sqrt(maps['star_sigma_ivar']) < 10, 
+    #                           star_sigma_mask)
 
     mstar_sigma = ma.array(star_sigma, mask=star_sigma_mask)
     mstar_sigma_ivar = ma.array(star_sigma_ivar, mask=star_sigma_mask)
@@ -40,7 +194,7 @@ def median_star_sigma(gal_ID, star_sigma, star_sigma_ivar,
 
     mstar_sigma_corrected = np.sqrt(mstar_sigma**2 - mstar_sigma_corr**2)
     plot_sigma_map(gal_ID, mstar_sigma, IMAGE_DIR=IMAGE_DIR, IMAGE_FORMAT=IMAGE_FORMAT)
-    plot_sigma_map(gal_ID, mstar_sigma_corr, corr=True, IMAGE_DIR=IMAGE_DIR, 
+    plot_sigma_map(gal_ID, mstar_sigma_corrected, corr=True, IMAGE_DIR=IMAGE_DIR, 
                    IMAGE_FORMAT=IMAGE_FORMAT)
 
     ############################################################################
@@ -54,33 +208,105 @@ def median_star_sigma(gal_ID, star_sigma, star_sigma_ivar,
     star_sigma_med_err = ma.std(mstar_sigma_corrected) * \
         np.sqrt(np.pi * (2*n+1)/(4*n))
     
-    return star_sigma_med, star_sigma_med_err, mstar_sigma, mstar_sigma_corr
+    
+    return star_sigma_med, star_sigma_med_err, mstar_sigma, mstar_sigma_corrected
 
 
 
 def calculate_virial_mass(gal_ID, star_sigma, star_sigma_ivar, 
                           star_sigma_corr, star_sigma_mask, flux_map,
+                          sMass_density, sMass_density_err,
                            Ha_vel, Ha_vel_mask, r50, z, 
                           IMAGE_DIR=None, IMAGE_FORMAT='png' ):
     '''
-    calculate virial mass 
+    calculate virial mass using the stellar mass weighted mean weighted velocity
+    dispersion
+
+    plot diagnostic plots for each galaxy 
 
     PARAMETERS
     ==========
+    gal_ID : string
+        galaxy plate-ifu
 
+    star_sigma : array
+        stellar velocity dispersion map [km/s]
+    
+    star_sigma_ivar : array
+        stellar velocity dispersion inverse variance map
+
+    star_sigma_corr : array
+        stellar velocity dispersion correction map
+
+    star_sigma_mask : array
+        stellar velocity dispersion default mask
+
+    flux_map : array
+        g-band weighted mean flux map
+
+    sMass_density : array
+        stellar mass density map [log(M_sun/spax^2)]
+
+    sMass_density_err : array
+        stellar mass density uncertainty map
+
+    Ha_vel : array
+        H-alpha velocity map
+
+    Ha_vel_mask : array
+        H-alpha velocity map mask
+
+    r50 : float
+        Petrosian half-light radius [arcseconds]
+
+    z : float
+        nsa redshift
+    
+    IMAGE_DIR : string
+        directory to save figures, default is None
+
+    IMAGE_FORMAT : string
+        format to save figures, deafault is 'png'
+
+    RETURNS
+    =======
+    logMvir : float
+        virial mass of galaxy [log(M_sun)]
+
+    logMvir_err : float
+        uncertainty on logMvir
+
+    x : float
+        stellar mass weighted mean velocity dispersion [km/s]
+
+    x_err : float
+        uncertainty on x
+
+    
     '''
 
     ############################################################################
     # get median velocity dispersion and uncertainty
     ############################################################################
 
-    star_sigma_med, star_sigma_med_err, mstar_sigma, mstar_sigma_corr = median_star_sigma(gal_ID,
-                                                           star_sigma, 
-                                                           star_sigma_ivar, 
-                                                           star_sigma_corr, 
-                                                           star_sigma_mask,
-                                                           IMAGE_DIR,
-                                                           IMAGE_FORMAT)
+    # star_sigma_med, star_sigma_med_err, mstar_sigma, mstar_sigma_corrected = median_star_sigma(gal_ID,
+    #                                                        star_sigma, 
+    #                                                        star_sigma_ivar, 
+    #                                                        star_sigma_corr, 
+    #                                                        star_sigma_mask,
+    #                                                        IMAGE_DIR,
+    #                                                        IMAGE_FORMAT)
+    
+    x2, x2_err2, mstar_sigma, mstar_sigma_corrected = \
+        sMass_weighted_star_sigma(gal_ID,
+                                  star_sigma, 
+                                  star_sigma_ivar, 
+                                  star_sigma_corr, 
+                                  star_sigma_mask,
+                                  sMass_density,
+                                  sMass_density_err,
+                                  IMAGE_DIR,
+                                  IMAGE_FORMAT)
     
     ############################################################################
     # plot photometric image
@@ -95,7 +321,7 @@ def calculate_virial_mass(gal_ID, star_sigma, star_sigma_ivar,
 
     plot_vel(mvel, gal_ID, IMAGE_DIR=IMAGE_DIR, IMAGE_FORMAT=IMAGE_FORMAT)
 
-    plot_diagnostic_panel(flux_map, mstar_sigma, mstar_sigma_corr, mvel, gal_ID, 
+    plot_diagnostic_panel(flux_map, mstar_sigma, mstar_sigma_corrected, mvel, gal_ID, 
                           IMAGE_DIR, IMAGE_FORMAT )
 
 
@@ -112,7 +338,13 @@ def calculate_virial_mass(gal_ID, star_sigma, star_sigma_ivar,
     # calculate mass and uncertainty
     ############################################################################
 
-    logMvir = np.log10(7.5 * r50_kpc * star_sigma_med**2 / G)
-    logMvir_err = 2 / (star_sigma_med * np.log(10)) * star_sigma_med_err
+    # logMvir = np.log10(7.5 * r50_kpc * star_sigma_med**2 / G)
+    # logMvir_err = 2 / (star_sigma_med * np.log(10)) * star_sigma_med_err
+    
+    x = np.sqrt(x2)
+    x_err = np.sqrt(x2_err2/(4*x2))
+    logMvir = np.log10(7.5 * r50_kpc * x2 / G)
+    logMvir_err = 2 / (x * np.log(10)) * x_err
 
-    return logMvir, logMvir_err, star_sigma_med, star_sigma_med_err
+
+    return logMvir, logMvir_err, x, x_err
--- ellipticals/elliptical_virial_mass_main.py
+++ ellipticals/elliptical_virial_mass_main.py
@@ -9,51 +9,70 @@ import numpy as np
 from astropy.table import Table
 import astropy.units as u
 
-from IO_data import extract_data
+from IO_data import extract_data, add_cols, extract_Pipe3d_data
 from elliptical_virial_mass import calculate_virial_mass
+
+# bluehive 
+# sys.path.insert(1, '/scratch/nravi3/RotationCurves/')
 
 sys.path.insert(1, '/Users/nityaravi/Documents/GitHub/RotationCurves/')
 from mapSmoothness_functions import how_smooth
 
 
-'''FILE_IDS = ['11011-1901','10223-9101','9877-12704','11760-9101','11953-1902',
-            '9490-9101','11830-9101','8319-6101','8592-1901','8325-3704',
-            '9878-6103','8716-1902','8143-3704','12514-6101','10837-1901',
-            '9487-12705','8133-6103','9887-1901','12067-3704','8274-6103',
-            '8134-3702','8085-6101','9089-1902','9488-6104','11953-6102',
-            '11836-3702','12678-6104','11865-9101','12620-1901','8319-1902',
-            '9039-3703','12485-6104','9090-6103','8551-6103','11950-12703',
-            '11826-6104','11836-3703','8625-3702','8614-12703','12067-3701',
-            '8093-9101','8330-3703','11828-3701','11969-9101','8248-1902',
-            '8937-3702','10840-6102','8481-9101','9862-6101','8149-12704',
-            '8488-6101','11946-3704','9512-6104','11969-9102','8330-12705',
-            '9868-6104','8566-6102','8241-3701','8612-6103','10837-6101',
-            '12073-3703','9502-12701','9186-1902','11004-9102','11011-3701',
-            '9506-6104','11024-12702','10501-3701','8158-3702','8482-12704',
-            '10511-6104','8710-1901','8655-3703','11757-6102','9875-12705',
-            '10513-1902','8598-6101','7975-3703','8939-3701','8550-1901',
-            '7959-6104','8456-6103','11828-6104','10220-1902','8725-1902',
-            '9863-3704','8710-1901','8259-1901','9002-9101','11868-12702',
-            '11748-6102','8454-12705','8310-3701','11017-1902','9502-6103',
-            '11948-12705','10495-3703','11836-12703','10498-3703','8262-6102']'''
+# FILE_IDS = ['11011-1901','10223-9101','9877-12704','11760-9101','11953-1902',
+#             '9490-9101','11830-9101','8319-6101','8592-1901','8325-3704',
+#             '9878-6103','8716-1902','8143-3704','12514-6101','10837-1901',
+#             '9487-12705','8133-6103','9887-1901','12067-3704','8274-6103',
+#             '8134-3702','8085-6101','9089-1902','9488-6104','11953-6102',
+#             '11836-3702','12678-6104','11865-9101','12620-1901','8319-1902',
+#             '9039-3703','12485-6104','9090-6103','8551-6103','11950-12703',
+#             '11826-6104','11836-3703','8625-3702','8614-12703','12067-3701',
+#             '8093-9101','8330-3703','11828-3701','11969-9101','8248-1902',
+#             '8937-3702','10840-6102','8481-9101','9862-6101','8149-12704',
+#             '8488-6101','11946-3704','9512-6104','11969-9102','8330-12705',
+#             '9868-6104','8566-6102','8241-3701','8612-6103','10837-6101',
+#             '12073-3703','9502-12701','9186-1902','11004-9102','11011-3701',
+#             '9506-6104','11024-12702','10501-3701','8158-3702','8482-12704',
+#             '10511-6104','8710-1901','8655-3703','11757-6102','9875-12705',
+#             '10513-1902','8598-6101','7975-3703','8939-3701','8550-1901',
+#             '7959-6104','8456-6103','11828-6104','10220-1902','8725-1902',
+#             '9863-3704','8710-1901','8259-1901','9002-9101','11868-12702',
+#             '11748-6102','8454-12705','8310-3701','11017-1902','9502-6103',
+#             '11948-12705','10495-3703','11836-12703','10498-3703','8262-6102']
 
 FILE_IDS = ['11011-1901']
 
-
-
 IMAGE_FORMAT = 'png'
 
+################################################################################
+# Paths for Nitya Macbook
+################################################################################
 
 MANGA_FOLDER = '/Users/nityaravi/Documents/Research/RotationCurves/data/manga/'
 IMAGE_DIR = MANGA_FOLDER + 'Ellipticals_Images/'
 MAP_FOLDER = MANGA_FOLDER + 'DR17/'
-
-
+PIPE3D_FOLDER = MANGA_FOLDER +'Pipe3D/'
 DRP_FILENAME = MANGA_FOLDER + 'output_files/DR17/CURRENT_MASTER_TABLE/Elliptical_StelVelDisp.fits'
 NSA_FILENAME = '/Users/nityaravi/Documents/Research/RotationCurves/data/nsa_v1_0_1.fits'
 
+################################################################################
+# Paths for Bluehive
+################################################################################
+
+# MANGA_FOLDER = '/scratch/kdougla7/data/SDSS/dr17/manga/spectro/'
+# IMAGE_DIR = '/scratch/nravi3/ellipticals/'
+# MAP_FOLDER = MANGA_FOLDER + 'analysis/v3_1_1/3.1.0/HYB10-MILESHC-MASTARSSP/'
+# PIPE3D_FOLDER = MANGA_FOLDER + 'pipe3d/'
+# #update
+# DRP_FILENAME = '/scratch/nravi3/disk_masses_HIdr3_err_morph_v2.fits'
+# NSA_FILENAME = '/scratch/kdougla7/data/NSA/nsa_v1_0_1.fits'
+
+
+################################################################################
+################################################################################
 
 map_smoothness_min = 2.0
+CLEAR_COLS = True  # zeros out columns in table
 
 START = datetime.datetime.now()
 
@@ -70,6 +89,14 @@ for i in range(len(DRP_table)):
 
     DRP_index[gal_ID] = i
 ################################################################################
+
+if CLEAR_COLS:
+    DRP_table = add_cols(DRP_table, ['smoothness_score', 
+                                     'Mvir', 
+                                     'Mvir_err',
+                                     'star_sigma',
+                                     'star_sigma_err'])
+
     
     
 for gal_ID in FILE_IDS:
@@ -97,6 +124,8 @@ for gal_ID in FILE_IDS:
             print('\n')
             continue
 
+        pipe3d_maps = extract_Pipe3d_data(PIPE3D_FOLDER,gal_ID,'sMass')
+
         print( gal_ID, "extracted")
 
     
@@ -112,7 +141,8 @@ for gal_ID in FILE_IDS:
                                                 )
                                                 )
         if (map_smoothness < map_smoothness_min) and (map_smoothness_5sigma < map_smoothness_min):
-            print('map is too smooth\n')
+            print('map is not smooth\n')
+            continue
 
         print('smoothness score: ', map_smoothness, '\n')
         ########################################################################
@@ -128,6 +158,8 @@ for gal_ID in FILE_IDS:
                                                                            maps['star_sigma_corr'],
                                                                            maps['star_sigma_mask'],
                                                                            maps['mflux'],
+                                                                           pipe3d_maps['sMass_density'],
+                                                                           pipe3d_maps['sMass_density_err'],
                                                                            maps['Ha_vel'],
                                                                            maps['Ha_vel_mask'],
                                                                            r50,
@@ -143,6 +175,7 @@ for gal_ID in FILE_IDS:
         DRP_table['Mvir_err'][i_DRP] = Mvir_err
         DRP_table['star_sigma'][i_DRP] = star_sigma
         DRP_table['star_sigma_err'][i_DRP] = star_sigma_err
+        DRP_table['smoothness_score'][i_DRP] = map_smoothness
 
         calc_time = datetime.datetime.now() - start
 
@@ -151,7 +184,9 @@ for gal_ID in FILE_IDS:
         print('Mvir: ', Mvir, ', Mvir_err: ', Mvir_err, '\n')
         print('star_sigma: ', star_sigma, ', star_sigma_err: ', star_sigma_err)
 
-DRP_table.write(DRP_FILENAME[:-4] + '_Mvir.fits', overwrite=True, format='fits')
+        DRP_table.write(DRP_FILENAME[:-5] + 'DAPMeanSigma_Mvir_smoothness_lt_2.fits', overwrite=True, format='fits')
+
+DRP_table.write(DRP_FILENAME[:-5] + 'DAPMeanSigma_Mvir_smoothness_lt_2.fits', overwrite=True, format='fits')
 
 TIME = datetime.datetime.now() - START
 print('total time: ', TIME)

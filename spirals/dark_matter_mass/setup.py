83b1cc2ddae7841fee27a78faa3deca97888d505
Nitya Ravi <nityarvi@gmail.com>
2025-07-31 21:27:44 -0400

    modified for newer python versions

--- spirals/dark_matter_mass/setup.py
+++ spirals/dark_matter_mass/setup.py
@@ -5,7 +5,10 @@ import numpy
 
 setup(
       name='dark_matter_mass',
-      ext_modules=cythonize("dark_matter_mass_v1_cython.pyx"),
-      include_dirs=[numpy.get_include()],
-      compiler_directives={'language_level' : "3"} # added for python3
+      ext_modules=cythonize("dark_matter_mass_v1_cython.pyx",
+      compiler_directives={'cpow' : True,
+                            'language_level' : "3",  # added for python3
+                           }
+      ),
+      include_dirs=[numpy.get_include()]
       )

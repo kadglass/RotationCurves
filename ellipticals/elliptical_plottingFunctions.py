dd36d5aa78721dbf8d1fa939490aa0ea1fe31afd
Nitya Ravi <nityarvi@gmail.com>
2026-04-28 14:54:54 -0400

    changed gaussian fit

--- ellipticals/elliptical_plottingFunctions.py
+++ ellipticals/elliptical_plottingFunctions.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 from scipy.optimize import curve_fit
+import scipy.stats as stats
 
 from elliptical_stellar_mass_functions import exponential_sphere,\
                                              exponential_sphere_disk,\
@@ -737,7 +738,7 @@ def med_percentile_scaled(vals, logscale=True, return_logscale=True):
     return p
 
 
-def gaussian(x, A, x0, sigma):
+def gaussian(x, x0, sigma):
     '''
     normal dist
 
@@ -746,8 +747,6 @@ def gaussian(x, A, x0, sigma):
 
     x : array
         data
-    A : float
-        coefficient
     x0 : float
         mean
     sigma : float
@@ -758,10 +757,10 @@ def gaussian(x, A, x0, sigma):
     gaussian w parameters
     '''
 
-    return A * np.exp(-(x-x0)**2/(2*sigma**2))
+    return 1/(np.sqrt(2*np.pi) * sigma) * np.exp(-(x-x0)**2/(2*sigma**2))
 
 def fit_to_gaussian(binned_data, plot_dir='', plot_name='', bin_lab='' , bin_order='norm',plot_bins=[], xlab='', 
-                    use_custom_bins=False, custom_bins=[]):
+                    use_custom_bins=False, custom_bins=[], median_guess=False):
     '''
     fit gaussian dist to data and plot data, fit. bins must have at least 10
     data pts
@@ -812,12 +811,14 @@ def fit_to_gaussian(binned_data, plot_dir='', plot_name='', bin_lab='' , bin_ord
         data = binned_data[i]
         
         if use_custom_bins:
-            counts, bins = np.histogram(data, bins=custom_bins[i])
+            counts, bins = np.histogram(data, bins=custom_bins[i], density=True)
         else:
-            counts, bins = np.histogram(data, bins=int(np.sqrt(len(data))+1))
+            counts, bins = np.histogram(data, bins=int(np.sqrt(len(data))+1), density=True)
         bin_ctrs = (bins[1:] + bins[:-1])/2
 
-        p0 = [np.max(counts), np.mean(data), np.std(data)]
+        p0 = [ np.median(data), np.std(data)]
+        if median_guess:
+            p0 = [np.median(data), stats.median_abs_deviation(data)]
 
         if len(data) >= 10:
 
@@ -825,22 +826,37 @@ def fit_to_gaussian(binned_data, plot_dir='', plot_name='', bin_lab='' , bin_ord
             try:
     
                 popt, pcov = curve_fit(gaussian, bin_ctrs, counts, p0=p0)
+                # mu, sig = stats.norm.fit(data)
     
             except:
     
                 print(f'failed on bin {i}')
                 continue
     
-            avgs[i] = popt[1]
-            sigmas[i] = popt[2]/np.sqrt(len(data))
+            avgs[i] = popt[0]
+            # sigmas[i] = popt[2]/np.sqrt(len(data))
+            sigmas[i] = popt[1]
+
+            # avgs[i] = mu
+            # sigmas[i] = sig
     
             
 
             xs = np.linspace(bins[0], bins[-1], 100)
-            plt.plot(xs, gaussian(xs, popt[0], popt[1], popt[2]), color='r', label=fr'$\mu$ = {popt[1]:.2}, $\sigma$ = {popt[2]:.2}')
+            plt.plot(xs, gaussian(xs, popt[0], popt[1]), color='r', label=fr'$\mu$ = {popt[0]:.2}, $\sigma$ = {popt[1]:.2}')
+            # plt.plot(xs, len(data)*(bin_ctrs[1] - bin_ctrs[0])*stats.norm.pdf(xs, mu, sig), 
+                    #  color='r', label=fr'$\mu$ = {mu:.2}, $\sigma$ = {sig:.2}')
+
             plt.xlabel(xlab, fontsize=14)
-            plt.scatter(bin_ctrs, counts, marker='.', color='k')
-    
+        
+            # plt.scatter(bin_ctrs, counts, 
+            #         #  marker='.', color='k'
+            #          )
+
+            # plt.hist(bins[:-1], bins, weights=counts)
+
+            plt.stairs(counts, bins)
+
             # plt.title(plot_name + f'_bin_{i}')
 
             if bin_order=='flip':
@@ -855,5 +871,8 @@ def fit_to_gaussian(binned_data, plot_dir='', plot_name='', bin_lab='' , bin_ord
             plt.tight_layout()
             plt.savefig(plot_dir + plot_name + f'_bin_{i}' + '.png', bbox_inches='tight')
             plt.close()
+
+        else:
+            print(f'not enough data points in bin {i}')
 
     return avgs, sigmas
